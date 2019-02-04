2ef3b52a6b305dd5b96bb1762b808168b7604f42
Mike Green <mike.green@simplethread.com>
2019-02-04 12:54:41 -0500

    Fix issues with registration validation, add controller action code and form fields to create users w/ credentials

--- lib/rumbl/accounts/credential.ex
+++ lib/rumbl/accounts/credential.ex
@@ -15,14 +15,14 @@ defmodule Rumbl.Accounts.Credential do
   @doc false
   def changeset(credential, attrs) do
     credential
-    |> cast(attrs, [:email, :password_hash])
-    |> validate_required([:email, :password_hash])
-    |> validate_length(:passsword, min: 6, max: 100)
+    |> cast(attrs, [:email, :password])
+    |> validate_required([:email, :password])
+    |> validate_length(:password, min: 6, max: 100)
     |> unique_constraint(:email)
     |> put_pass_hash()
   end
 
-  def put_pass_hash(changeset) do
+  defp put_pass_hash(changeset) do
     case changeset do
       %Ecto.Changeset{valid?: true, changes: %{password: pass}} ->
         put_change(changeset, :password_hash, Comeonin.Pbkdf2.hashpwsalt(pass))
