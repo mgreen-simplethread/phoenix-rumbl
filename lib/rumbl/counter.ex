eeab76e59382dcbbe4c4d04ac37bef890e90e63d
Mike Green <mike.green@simplethread.com>
2019-02-11 14:27:59 -0500

    Rewrite counter app to use GenServer

--- lib/rumbl/counter.ex
+++ lib/rumbl/counter.ex
@@ -4,46 +4,45 @@ defmodule Rumbl.Counter do
   of a numeric counter, which runs in an isolated process and is interacted with via messages from its parent
   """
 
+  use GenServer
+
   @doc """
   Increment the counter's value
   """
-  def inc(pid), do: send(pid, :inc)
+  def inc(pid), do: GenServer.cast(pid, :inc)
 
   @doc """
   Decrement the counter's value
   """
-  def dec(pid), do: send(pid, :dec)
+  def dec(pid), do: GenServer.cast(pid, :dec)
 
   @doc """
   Request the current counter value
   """
-  def val(pid, timeout \\ 5000) do
-    ref = make_ref()
-    send(pid, {:val, self(), ref})
-
-    receive do
-      {^ref, val} -> val
-    after
-      timeout -> exit(:timeout)
-    end
+  def val(pid) do
+    GenServer.call(pid, :val)
   end
 
   @doc """
   Start the counter process with an initial value
   """
   def start_link(initial_val) do
-    {:ok, spawn_link(fn -> listen(initial_val) end)}
+    GenServer.start_link(__MODULE__, initial_val)
   end
 
-  defp listen(val) do
-    receive do
-      :inc ->
-        listen(val + 1)
-      :dec ->
-        listen(val - 1)
-      {:val, sender, ref} ->
-        send(sender, {ref, val})
-        listen(val)
-    end
+  def init(initial_val) do
+    {:ok, initial_val}
+  end
+
+  def handle_cast(:inc, val) do
+    {:noreply, val + 1}
+  end
+
+  def handle_cast(:dec, val) do
+    {:noreply, val - 1}
+  end
+
+  def handle_call(:val, _from, val) do
+    {:reply, val, val}
   end
 end
